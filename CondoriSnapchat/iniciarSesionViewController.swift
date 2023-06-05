d78bad5663108c3f77972704f18319de3c5520bd
sesentayseis <carlos.condori.c@tecsup.edu.pe>
2023-06-04 20:02:54 -0500

    Se agrego el botón para ingresar con numero

--- CondoriSnapchat/iniciarSesionViewController.swift
+++ CondoriSnapchat/iniciarSesionViewController.swift
@@ -1,6 +1,7 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseStorageUI
 
 
 class iniciarSesionViewController: UIViewController {
@@ -25,6 +26,64 @@ class iniciarSesionViewController: UIViewController {
         }
     }
     }
+//MARK: Ingresar mediante numero
+    @IBAction func capturarNumeroTapped(_ sender: Any) {
+        
+        guard let phoneNumber = emailTextField.text else {
+                return
+        }
+            
+        verifyPhoneNumber(phoneNumber: phoneNumber)
+    }
     
+    func verifyPhoneNumber(phoneNumber: String) {
+        PhoneAuthProvider.provider().verifyPhoneNumber(phoneNumber, uiDelegate: nil) { (verificationID, error) in
+            if let error = error {
+                print("Error al enviar el código de verificación: \(error.localizedDescription)")
+                return
+            }
+            
+            // Guarda el verificationID para usarlo posteriormente
+            
+            // Presenta la pantalla para ingresar el código de verificación
+            // Aquí puedes mostrar una nueva vista o presentar un alert para que el usuario ingrese el código de verificación
+            // Puedes pasar el verificationID a la siguiente vista o mantenerlo en una propiedad de la clase para usarlo después
+            self.showVerificationCodeScreen(verificationID: verificationID ?? "")
+        }
+    }
+
+    func showVerificationCodeScreen(verificationID: String) {
+        let alertController = UIAlertController(title: "Verificar Código", message: "Ingrese el código de verificación", preferredStyle: .alert)
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Código de Verificación"
+        }
+        
+        let confirmAction = UIAlertAction(title: "Confirmar", style: .default) { (_) in
+            if let verificationCode = alertController.textFields?.first?.text {
+                self.signInWithVerificationCode(verificationID: verificationID, verificationCode: verificationCode)
+            }
+        }
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        
+        alertController.addAction(confirmAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func signInWithVerificationCode(verificationID: String, verificationCode: String) {
+        let credential = PhoneAuthProvider.provider().credential(withVerificationID: verificationID, verificationCode: verificationCode)
+
+        Auth.auth().signIn(with: credential) { (authResult, error) in
+            if let error = error {
+                print("Error al iniciar sesión con número de teléfono: \(error.localizedDescription)")
+                return
+            }
+            
+            // Inicio de sesión exitoso
+            print("Inicio de sesión exitoso para el número: \(authResult?.user.phoneNumber ?? "")")
+        }
+    }
+
 }
 
--- Podfile
+++ Podfile
@@ -1,20 +1,24 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+#platform :ios, '15.0'
 
 target 'CondoriSnapchat' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
-  # Pods for CondoriSnapchat
-  # Add the Firebase pod for Google Analytics
 pod 'FirebaseAnalytics'
 
-# For Analytics without IDFA collection capability, use this pod instead
-# pod ‘Firebase/AnalyticsWithoutAdIdSupport’
-
-# Add the pods for any other Firebase products you want to use in your app
-# For example, to use Firebase Authentication and Cloud Firestore
 pod 'FirebaseAuth'
 pod 'FirebaseFirestore'
+pod 'FirebaseUI/Storage' 
 
+
+end
+post_install do |installer|
+    installer.generated_projects.each do |project|
+          project.targets.each do |target|
+              target.build_configurations.each do |config|
+                  config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '13.0'
+               end
+          end
+   end
 end
