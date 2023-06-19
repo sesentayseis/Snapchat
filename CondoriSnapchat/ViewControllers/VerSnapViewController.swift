c6bfb37113f23f8d3358b2de2bf1481d273bfd60
sesentayseis <carlos.condori.c@tecsup.edu.pe>
2023-06-19 03:41:12 -0500

    tarea13.43

---
+++ CondoriSnapchat/ViewControllers/VerSnapViewController.swift
@@ -0,0 +1,20 @@
+
+import UIKit
+import SDWebImage
+
+class VerSnapViewController: UIViewController{
+    @IBOutlet weak var lblMensaje: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    var snap = Snap()
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        lblMensaje.text = "Mensaje: " + snap.descrip
+        imageView.sd_setImage(with: URL(string: snap.imagenURL), completed: nil)
+    }
+    
+    
+    
+
+}
